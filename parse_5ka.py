52fb0728e0974d8de33b5d077a4f839335392b9a
Roman Tseiko <rtseiko@deloitte.com>
2021-04-05 09:38:13 +0300

    Урок 1. Основы клиент-серверного взаимодействия. Парсинг API

---
+++ parse_5ka.py
@@ -0,0 +1,74 @@
+import time
+import json
+from pathlib import Path
+import requests
+
+
+class Parse5ka:
+    headers = {"User-Agent": "FILIPP KIRKOROV"}
+
+    def __init__(self, start_url: str, save_path: Path):
+        self.start_url = start_url
+        self.save_path = save_path
+
+    def _get_response(self, url):
+        while True:
+            response = requests.get(url, headers=self.headers)
+            if response.status_code == 200:
+                return response
+            time.sleep(0.5)
+
+    def run(self):
+        for product in self._parse(self.start_url):
+            product_path = self.save_path.joinpath(f"{product['id']}.json")
+            self._save(product, product_path)
+
+    def _parse(self, url: str):
+        while url:
+            response = self._get_response(url)
+            data: dict = response.json()
+            url = data["next"]
+            for product in data["results"]:
+                yield product
+
+    def _save(self, data: dict, file_path: Path):
+        file_path.write_text(json.dumps(data, ensure_ascii=False))
+
+
+class CategoriesParser(Parse5ka):
+    def __init__(self, categories_url, *args, **kwargs):
+        self.categories_url = categories_url
+        super().__init__(*args, **kwargs)
+
+    def _get_categories(self):
+        response = self._get_response(self.categories_url)
+        data = response.json()
+        return data
+
+    def run(self):
+        for category in self._get_categories():
+            category["products"] = []
+            params = f"?categories={category['parent_group_code']}"
+            url = f"{self.start_url}{params}"
+
+            category["products"].extend(list(self._parse(url)))
+            file_name = f"{category['parent_group_code']}.json"
+            cat_path = self.save_path.joinpath(file_name)
+            self._save(category, cat_path)
+
+
+def get_save_path(dir_name):
+    save_path = Path(__file__).parent.joinpath(dir_name)
+    if not save_path.exists():
+        save_path.mkdir()
+    return save_path
+
+
+if __name__ == "__main__":
+    url = "https://5ka.ru/api/v2/special_offers/"
+    cat_url = "https://5ka.ru/api/v2/categories/"
+    save_path_products = get_save_path("products")
+    save_path_categories = get_save_path("categories")
+    parser_products = Parse5ka(url, save_path_products)
+    cat_parser = CategoriesParser(cat_url, url, save_path_categories)
+    cat_parser.run()
